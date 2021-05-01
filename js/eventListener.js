707f464bd8771f3285b2d9da6372bce1696910cc
mia23ali <izzuddin.binali@gmail.com>
2021-05-01 13:06:57 +0800

    less ooga booga playaudio

--- js/eventListener.js
+++ js/eventListener.js
@@ -1,31 +1,107 @@
 //declaration
 
-let guling = document.querySelectorAll(".pots");
+let guling = document.querySelectorAll(".pots:not(empty)");
 
 //let keyToggle = document.getElementById("keyToggle");
 
 //let keys = document.querySelectorAll(".key");
 
-let notes=document.querySelectorAll(".notes");
+let notes=[
+    {
+        noteId: "Db4",
+        note: "./assets/gulingtangan-notes/Db4.wav"
+    }
+    ,   
+    {
+        noteId: "Eb4",
+        note: "./assets/gulingtangan-notes/Eb4.wav"
+    }
+    ,
+    {
+        noteId: "Gb4",
+        note: "./assets/gulingtangan-notes/Gb4.wav"
+    }
+    ,
+    {
+        noteId: "Ab4",
+        note: "./assets/gulingtangan-notes/Ab4.wav"
+    }
+    ,
+    {
+        noteId: "Bb4",
+        note: "./assets/gulingtangan-notes/Bb4.wav"
+    }
+    ,
+    {
+        noteId: "C4",
+        note: "./assets/gulingtangan-notes/C4.wav"
+    }
+    ,
+    {
+        noteId: "D4",
+        note: "./assets/gulingtangan-notes/D4.wav"
+    }
+    ,
+    {
+        noteId: "E4",
+        note: "./assets/gulingtangan-notes/E4.wav"
+    }
+    ,
+    {
+        noteId: "F4",
+        note: "./assets/gulingtangan-notes/F4.wav"
+    }
+    ,
+    {
+        noteId: "G4",
+        note: "./assets/gulingtangan-notes/G4.wav"
+    }
+    ,
+    {
+        noteId: "A4",
+        note: "./assets/gulingtangan-notes/A4.wav"
+    }
+    ,
+    {
+        noteId: "B4",
+        note: "./assets/gulingtangan-notes/B4.wav"
+    }
+    ,
+    {
+        noteId: "C5",
+        note: "./assets/gulingtangan-notes/C4.wav"
+    }
+
+]
 
 //gulingtangan onclick
 
 guling.forEach(function(gulingtangan){
     gulingtangan.addEventListener("click", function(){
-      gulingtangan.children[1].play();
+//      gulingtangan.children[1].play();
+        for(i=0; i<notes.length;i++){
+            if((notes[i].noteId)===(gulingtangan.id)){
+                playNote(notes[i].note);
+            }
+        }
     })
 })
+
+function playNote(note){
+    let audio = new Audio(note);
+    audio.play();
+}
 
 //gulingtangan keypress
 
 document.addEventListener("keypress", logKey);
 
-let keyArray = ["", "1", "2", "", "7", "8", "9", "", "q", "w", "e", "r", "u", "i", "o", "p"];
+let keyArray = ["1", "2", "7", "8", "9", "q", "w", "e", "r", "u", "i", "o", "p"];
 
 function logKey(e){
     for(i=0; i<keyArray.length; i++){
         if(e.key === keyArray[i]){
-            notes[i].play();
+            playNote(notes[i].note);
         }
     }
 }
