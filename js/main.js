deab5f4cdb341d82ed2ee358aed14d64466774f9
akasmirhan4 <amirrul.kasmirhan@gmail.com>
2021-05-04 14:46:52 +0800

    added metronome; need the html

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
-const sampler = new Tone.Sampler({
-	urls: urlsList,
-	release: 1,
-	baseUrl: PATH,
+let currentMeasure = -1;
+
+const tick = new Tone.Player("../assets/tick.wav").toDestination();
+const tock = new Tone.Player("../assets/tock.wav").toDestination();
+
+
+const gulingtangan = new Tone.Sampler({
+    urls: urlsList,
+    release: 1,
+    baseUrl: PATH,
 }).toDestination();
+
+let metronome = function () {
+    Tone.Transport.scheduleRepeat((time) => {
+        // use the callback time to schedule events
+        let position = Tone.Transport.position;
+        let musicTime = position.split(':');
+        if(currentMeasure != musicTime[0]){
+            currentMeasure = musicTime[0];
+            tock.start();
+        }
+        else{
+            tick.start();
+        }
+    }, "4n");
+    // transport must be started before it starts invoking events
+    Tone.Transport.start();
+    Tone.start();
+};
+
+document.getElementById('metronone-play').onclick = metronome;
